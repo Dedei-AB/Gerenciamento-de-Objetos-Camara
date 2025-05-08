7cc7da0ebf82aa8a3e1ee99765035d5d02ada55c
Dedei-AB <andreiabners@gmail.com>
2025-05-08 16:55:35 -0300

    json do banco de dados

--- Gerenciamento/config/db.js
+++ Gerenciamento/config/db.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost", // Endereço do seu servidor MySQL
-    user: "root", // Usuário do banco de dados
-    password: "Mudar@1234", // Senha do banco de dados
-    database: "bancodeobjetos", // Nome do seu banco de dados
-  });
+  host: "localhost", // Endereço do seu servidor MySQL
+  user: "root", // Usuário do banco de dados
+  password: "Mudar@1234", // Senha do banco de dados
+  database: "bancodeobjetos", // Nome do seu banco de dados
+});
 
-  connection.connect((err)=>{if (err){ console.log("Erro ao conectar", err)}else{console.log("Conectado")}}) 
+connection.connect((err) => {
+  if (err) {
+    console.log("Erro ao conectar", err);
+  } else {
+    console.log("Conectado");
+  }
+});
 
-module.exports = connection
+module.exports = connection;
--- Gerenciamento/routes/objeto.js
+++ Gerenciamento/routes/objeto.js
@@ -1,33 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const connection = require('../config/db'); // Certifique-se de ter a conexão configurada corretamente
+const connection = require("../config/db"); // Certifique-se de ter a conexão configurada corretamente
+const controllers = require("../controllers/objetosController");
 
+router.get("/", controllers.getObjetos);
 
+router.get("/tipos-objeto", controllers.getTipoObjeto);
 
-// Rota para /tipos
-router.get("/tipos", (req, res) => {
-  connection.query("SELECT TipoObjeto_idTipoObjeto, codigo FROM bancodeobjetos.objeto;", (err, results) => {
-    if (err) {
-      console.error('Erro na consulta SQL:', err);  // Logando o erro no servidor
-      return res.status(500).json({ erro: "Erro ao buscar tipos de objeto" });  // Respondendo com erro
-    }
-    console.log('Resultados da consulta:', results);  // Exibindo resultados no servidor
-    res.send("json(results)");  // Enviando resultados como JSON
-  });
-});
-
-// routes/objeto.js
-router.post("/salvar-codigo", (req, res) => {
-    const { codigo, tipoObjeto, status, sala, complemento } = req.body;
-  
-    const sql = "INSERT INTO objeto (codigo, TipoObjeto_idTipoObjeto, status, sala, complemento) VALUES (?, ?, ?, ?, ?)";
-    connection.query(sql, [codigo, tipoObjeto, status, sala, complemento], (err, results) => {
-      if (err) {
-        console.error("Erro ao salvar objeto:", err);
-        return res.status(500).json({ erro: "Erro ao salvar objeto" });
-      }
-      res.json({ mensagem: "Objeto cadastrado com sucesso!" });
-    });
-  });
-  
-module.exports = router; 
+module.exports = router;
