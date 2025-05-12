ff88291ab60fdfe73d1c1b3cc64ed9d58cb47ef5
Dedei-AB <andreiabners@gmail.com>
2025-05-12 14:59:02 -0300

    busca de objetos

--- Gerenciamento/public/js/buscar.js
+++ Gerenciamento/public/js/buscar.js
@@ -1,9 +1,28 @@
-const objetos = async () => {
+const tabela = document.getElementById("tabela");
+
+async function pegarDados() {
   const res = await fetch("/dados-buscar");
   const dados = await res.json();
   return dados;
+}
+
+const nomeObjeto = async () => {
+  const dados = await pegarDados();
+  dados.forEach((element) => {
+    tabela.innerHTML += `<tr>
+              <td>${element.codigo}</td>
+              <td>${element.Complemento}</td>
+              <td>${element.Complemento}</td>
+              <td>${element.NomeSala}</td>
+              <td>${element.Nome}</td>
+            </tr>`;
+  });
 };
 
-objetos.forEach((element) => {
-  console.log(element.Complemento);
-});
+nomeObjeto();
+
+// Complemento: "monitor"
+// Nome: "ótimo"
+// NomeDoTipo: "monitor"
+// NomeSala: "COTEC"
+// codigo: 1234
