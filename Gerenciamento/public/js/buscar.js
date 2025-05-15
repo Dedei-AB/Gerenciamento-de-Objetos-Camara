64eb6a407833fe5a36eb91189e9e57d7d1529817
Dedei-AB <andreiabners@gmail.com>
2025-05-15 15:27:14 -0300

    Barra de pesquisa

--- Gerenciamento/public/js/buscar.js
+++ Gerenciamento/public/js/buscar.js
@@ -1,27 +1,51 @@
-const tabela = document.getElementById("tabela");
+const barraPesquisa = document.getElementById("barra-pesquisa");
+const corpoTabela = document.getElementById("corpo-tabela");
 
 async function pegarDados() {
   const res = await fetch("/dados-buscar");
   const dados = await res.json();
+  console.log("Dados", dados);
   return dados;
 }
 
-const nomeObjeto = async () => {
+async function mostrarObjetos() {
+  corpoTabela.innerHTML = ``;
   const dados = await pegarDados();
   dados.forEach((element) => {
-    tabela.innerHTML += `<tr>
+    corpoTabela.innerHTML += `<tr>
               <td>${element.codigo}</td>
               <td>${element.NomeDoTipo}</td>
-              <td>${element.Complemento}</td>
+              <td>${element.Complemento ? element.Complemento : "Sem nome"}</td>
               <td>${element.NomeSala}</td>
               <td>${element.Nome}</td>
             </tr>`;
   });
-};
-nomeObjeto();
+}
+mostrarObjetos();
 
-// Complemento: "monitor"
-// Nome: "ótimo"
-// NomeDoTipo: "monitor"
-// NomeSala: "COTEC"
-// codigo: 1234
+async function pesquisar(input) {
+  corpoTabela.innerHTML = ``;
+  const dados = await pegarDados();
+  const termo = input.toLowerCase();
+  const filtrados = dados.filter((item) =>
+    item.NomeDoTipo.toLowerCase().includes(termo)
+  );
+  filtrados.forEach((valor) => {
+    let novaLista = document.createElement("tr");
+    novaLista.innerHTML += `
+              <td>${valor.codigo}</td>
+              <td>${valor.NomeDoTipo}</td>
+              <td>${valor.Complemento ? valor.Complemento : "Sem nome"}</td>
+              <td>${valor.NomeSala}</td>
+              <td>${valor.Nome}</td>`;
+    corpoTabela.appendChild(novaLista);
+  });
+  console.log("Dados da pesquisa:", filtrados);
+}
+barraPesquisa.addEventListener("input", () => pesquisar(barraPesquisa.value));
+
+// 0: {codigo: 1234, NomeDoTipo: 'Computador', Nome: 'Ótimo', NomeSala: 'Sub. Macedo', Complemento: null}
+// 1: {codigo: 1111, NomeDoTipo: 'Impressora', Nome: 'Ótimo', NomeSala: 'Copa', Complemento: null}
+// 2: {codigo: 4444, NomeDoTipo: 'Computador', Nome: 'Ótimo', NomeSala: 'Tião Karate', Complemento: null}
+// 3: {codigo: 2222, NomeDoTipo: 'Monitor', Nome: 'Bom', NomeSala: 'COTEC', Complemento: null}
+// 4: {codigo: 3333, NomeDoTipo: 'Impressora', Nome: 'Ruim', NomeSala: 'Sala reunião', Complemento: null}
