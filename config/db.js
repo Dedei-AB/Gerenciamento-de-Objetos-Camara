e903a5d75c8ea4eb7fc6fe5032b23e1b454f27b9
Dedei-AB <andreiabners@gmail.com>
2025-06-05 16:21:41 -0300

    crtl z

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 const mysql = require("mysql2");
 
-
+const connection = mysql.createConnection({
+  host: "switchback.proxy.rlwy.net",
+  user: "root",
+  password: "HicNxVlAbsfTwOoBFeoPLvdPGKFgRbrM",
+  database: "railway",
+  port: 3306, // <- veja na Railway qual porta está definida
+  ssl: { rejectUnauthorized: false }
+});
 
 
 connection.connect((err) => {
