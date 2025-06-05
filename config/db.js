a0021eba20f524a055a15172b7552ab7f8322df3
Dedei-AB <andreiabners@gmail.com>
2025-06-05 16:09:08 -0300

    teste onrender

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost", // Endereço do seu servidor MySQL
-  user: "root", // Usuário do banco de dados
-  password: "Mudar@1234", // Senha do banco de dados
-  database: "bancodeobjetos", // Nome do seu banco de dados
+  host: "switchback.proxy.rlwy.net",
+  user: "root",
+  password: "HicNxVlAbsfTwOoBFeoPLvdPGKFgRbrM",
+  database: "railway",
+  port: 32630, // <- veja na Railway qual porta está definida
+  ssl: { rejectUnauthorized: false }
 });
+
 
 connection.connect((err) => {
   if (err) {
