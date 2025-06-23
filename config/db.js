0171a5f2997160ff89207d5f1a4685ed740ae4a5
Dedei-AB <andreiabners@gmail.com>
2025-06-23 14:33:22 -0300

    teste 7

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  port: process.env.DB_PORT || "3306",
+  user: process.env.DB_USER || "Admin",
+  password: process.env.DB_PASSWORD || "Mudar@1234",
+  database: process.env.DB_NAME || "bancodeobjetos",
 });
 
 connection.connect((err) => {
