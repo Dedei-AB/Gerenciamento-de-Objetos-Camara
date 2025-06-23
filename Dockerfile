203f5960d0311bb058bcb6bb9a06cb91297aebaf
Dedei-AB <andreiabners@gmail.com>
2025-06-23 14:45:33 -0300

    teste 7.1

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
 FROM node:18
 
 WORKDIR /app
+
+# Copia só package.json para instalar dependências primeiro
 COPY package*.json ./
+
 RUN npm install
+
+# Copia o resto do código
 COPY . .
+
 EXPOSE 3000
+
 CMD ["node", "app.js"]
--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.DB_PORT || "3306",
+  host: "localhost" || process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "Admin",
   password: process.env.DB_PASSWORD || "Mudar@1234",
   database: process.env.DB_NAME || "bancodeobjetos",
