baaf0129246e386e043762e4056bf1bc1b54c3b3
Dedei-AB <andreiabners@gmail.com>
2025-06-26 14:11:07 -0300

    pasta do backend

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const app = express();
 const objetosRoutes = require("./routes/objeto.js"); // Importando o arquivo de rotas
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("../public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", objetosRoutes);
 
 const PORT = 3000;
-const HOST = "0.0.0.0"; // escuta em todas as interfaces
+const HOST = "localhost"; // escuta em todas as interfaces
 
 app.listen(PORT, HOST, () => {
   console.log(`Servidor rodando em http://${HOST}:${PORT}`);
