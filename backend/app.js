3f5be5439576cf7cb92aa45a29b5be84475a94d9
Dedei-AB <andreiabners@gmail.com>
2025-06-26 16:07:05 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const objetosRoutes = require("./routes/objeto.js"); // Importando o arquivo de rotas
 
-app.use(express.json());
-app.use(express.static(__dirname, "public"));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/", objetosRoutes);
 
 const PORT = 3000;
-const HOST = "localhost"; // escuta em todas as interfaces
+const HOST = "0.0.0.0"; // escuta em todas as interfaces
 
 app.listen(PORT, HOST, () => {
   console.log(`Servidor rodando em http://${HOST}:${PORT}`);
